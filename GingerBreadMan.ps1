cec3a11a0faf3a669f4af1a1bd4a99d0d3332a1a
McBaldwin <McBaldwin@users.noreply.github.com>
2020-03-28 21:32:35 -0500

    Update GingerBreadMan.ps1

--- GingerBreadMan.ps1
+++ GingerBreadMan.ps1
@@ -1,6 +1,3 @@
-
-
-
 #Uncomment the below line to import a computer list from a text file
 $computers = get-content $psscriptroot\Dep_files\computers.txt
 #uncomment the below line to import a computer list from an OU
@@ -8,11 +5,10 @@ $computers = get-content $psscriptroot\Dep_files\computers.txt
 $events = Get-Content $PSScriptRoot\Dep_files\eventids.txt
 $date = Get-Date -Format dd-MMM-yy
 
-
+#checks if variable has a value
 if ($computers){
 foreach ($comp in $computers){
-    Get-EventLog -computename $comp -LogName Security | where {$events -contains $_.EventID } | select EventID,MachineName,index,EntryType,Message,Source,TimeGenerated | Export-csv -Path "$psscriptroot\$date.csv" -Append
-
+    Get-EventLog -computename $comp -LogName Security | where-object {$events -contains $_.EventID } | select-object EventID,MachineName,index,EntryType,Message,Source,TimeGenerated | Export-csv -Path "$psscriptroot\$date.csv" -Append
 
 }
 } else {
