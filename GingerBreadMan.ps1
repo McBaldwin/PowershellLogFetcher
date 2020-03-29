5b6631b6d5704b9cf9472d7df67819ffef2f58d3
McBaldwin <McBaldwin@users.noreply.github.com>
2020-03-28 21:25:59 -0500

    Initial files

---
+++ GingerBreadMan.ps1
@@ -0,0 +1,21 @@
+
+
+
+#Uncomment the below line to import a computer list from a text file
+$computers = get-content $psscriptroot\Dep_files\computers.txt
+#uncomment the below line to import a computer list from an OU
+#$computers = get-adcomputer -filter * 
+$events = Get-Content $PSScriptRoot\Dep_files\eventids.txt
+$date = Get-Date -Format dd-MMM-yy
+
+
+if ($computers){
+foreach ($comp in $computers){
+    Get-EventLog -computename $comp -LogName Security | where {$events -contains $_.EventID } | select EventID,MachineName,index,EntryType,Message,Source,TimeGenerated | Export-csv -Path "$psscriptroot\$date.csv" -Append
+
+
+}
+} else {
+    write-host "No computers were fetched, please ensure the text file path or computer OU is uncommented and properly identified" -foregroundcolor red
+
+}
