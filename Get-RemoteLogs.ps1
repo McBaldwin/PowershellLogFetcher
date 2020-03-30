296be424f4be5c3fc46be1e498c19171958adb0b
McBaldwin <McBaldwin@users.noreply.github.com>
2020-03-29 22:19:39 -0500

    Function
    Updated to split commands into functions

--- Get-RemoteLogs.ps1
+++ Get-RemoteLogs.ps1
@@ -1,11 +1,13 @@
 #Uncomment the below line to import a computer list from a text file
 $computers = get-content $psscriptroot\Dep_files\computers.txt
-#uncomment the below line to import a computer list from an OU
-#$computers = get-adcomputer -filter * 
+#uncomment the below line to import a computer list from an OU, note that you will need rsat installed for this to work
+#$computers = (get-adcomputer -filter * -searchbase "OU=Computers,DC=Mcbaldwin,DC=net").DNSHostName
+#$Lcomputers = get-content $psscriptroot\Dep_files\LinuxComputers.txt
 $events = Get-Content $PSScriptRoot\Dep_files\eventids.txt
 $date = Get-Date -Format dd-MMM-yy
 
 #checks if variable has a value
+Function WindowsLogFetch{
 if ($computers){
 foreach ($comp in $computers){
     write-host "Pulling logs from $comp" -foregroundcolor green
@@ -16,3 +18,17 @@ foreach ($comp in $computers){
     write-host "No computers were fetched, please ensure the text file path or computer OU is uncommented and properly identified" -foregroundcolor red
 
 }
+}
+
+
+
+function LinuxLogFetch{
+
+    if ($Lcomputers){
+        write-host "would now pull logs from linux"
+    }
+    
+}
+
+WindowsLogFetch
+#LinuxLogFetch
